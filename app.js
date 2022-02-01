c3783afc69db19ac1e3f9ae77d54fe3fa9fbd9f2
Carlos Lopez <serialmerol@gmail.com>
2022-02-01 10:21:29 -0600

    atributos guardados en local storage, comentarios extras para mejor comprension del codigo

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 
-let check = false;
-let checkLocal;
-let stat = 0;
-let char;
-let pers;
+//Variables globales
+let check = false; //checador generico
+let checkLocal; //checador de localStorage
+let stat = 0; //seleccionador de atributo a tirar dado
+let char; //setitem para datos de personaje en localStorage
+let pers; //getitem de datos de personaje
+let atriset; //setitem para atributos en localStorage
+let atriget; //getitem de atributos
 
 //Checador de localStorage
 if(localStorage.length > 0){
@@ -26,6 +29,14 @@ class Aventurero{
     }
 }
 
+//array para crear los atributos del personaje
+const personaje = [ str = {atributo: "Fuerza", valor: 0, modificador: 0,},
+                    dex = {atributo: "Destreza", valor: 0, modificador: 0},
+                    con = {atributo: "Constitucion", valor: 0, modificador: 0},
+                    int = {atributo: "Inteligencia", valor: 0, modificador: 0},
+                    wis = {atributo: "Sabiduria", valor: 0, modificador: 0},
+                    car = {atributo: "Carisma", valor: 0, modificador: 0}]
+
 //toma el booleano del checador de localStorage y dependiendo de la opcion toma los datos guardados o crea nuevo personaje
 if (checkLocal == false) {
     pers = JSON.parse( localStorage.getItem('char').split(","));
@@ -35,52 +46,44 @@ if (checkLocal == false) {
                             prompt("Ingresa la clase del personaje"));
     newChar.lvl(); //funcion para tomar dato de nivel y no salga de los parametros 
     localStorage.setItem('char', JSON.stringify(newChar))
+    for(i=0;i<6;i++){
+        do {
+            personaje[i].valor = prompt("Ingresa un valor entre 6 y 18 para tu " + personaje[i].atributo);
+            chequeo(personaje[i].valor,6,18); //chequeo de entrada de datos
+        } while (check == false);
+        mod(personaje[i].valor); //asignacion de modificadores
+    }
+    localStorage.setItem('atriset',JSON.stringify(personaje));
 }
                             
 show(); //toma los datos guardados, crea divs y los muestra en sus respectivos lugares
-
-//array para crear lps atributs del personaje
-const personaje = [ str = {atributo: "Fuerza", valor: 0, modificador: 0,},
-                    dex = {atributo: "Destreza", valor: 0, modificador: 0},
-                    con = {atributo: "Constitucion", valor: 0, modificador: 0},
-                    int = {atributo: "Inteligencia", valor: 0, modificador: 0},
-                    wis = {atributo: "Sabiduria", valor: 0, modificador: 0},
-                    car = {atributo: "Carisma", valor: 0, modificador: 0}]
-for(i=0;i<6;i++){
-    do {
-        personaje[i].valor = prompt("Ingresa un valor entre 6 y 18 para tu " + personaje[i].atributo);
-        chequeo(personaje[i].valor,6,18); //chequeo de entrada de datos
-    } while (check == false);
-    mod(personaje[i].valor); //asignacion de modificadores
-    }
-
 showAtribute(); //funcion que muestra atributos y modificadores
 
 do {
     selector(); //Selecciona el atributo para el que tiraran los dados
     switch(stat){
         case 0:
-            dados(personaje[stat].atributo, personaje[stat].valor, personaje[stat].modificador);
+            dados(atriget[stat].atributo, atriget[stat].valor, atriget[stat].modificador);
             tirada();
             break;
         case 1:
-            dados(personaje[stat].atributo, personaje[stat].valor, personaje[stat].modificador);
+            dados(atriget[stat].atributo, atriget[stat].valor, atriget[stat].modificador);
             tirada();
             break;
         case 2:
-            dados(personaje[stat].atributo, personaje[stat].valor, personaje[stat].modificador);
+            dados(atriget[stat].atributo, atriget[stat].valor, atriget[stat].modificador);
             tirada();
             break;
         case 3:
-            dados(personaje[stat].atributo, personaje[stat].valor, personaje[stat].modificador);
+            dados(atriget[stat].atributo, atriget[stat].valor, atriget[stat].modificador);
             tirada();
             break;    
         case 4:
-            dados(personaje[stat].atributo, personaje[stat].valor, personaje[stat].modificador);
+            dados(atriget[stat].atributo, atriget[stat].valor, atriget[stat].modificador);
             tirada();
             break;
         case 5:
-            dados(personaje[stat].atributo, personaje[stat].valor, personaje[stat].modificador);
+            dados(atriget[stat].atributo, atriget[stat].valor, atriget[stat].modificador);
             tirada();
             break;
     }
@@ -115,43 +118,44 @@ function show(){
 
 //Crea los nodos de atributos y crea los nodos de modificadores
 function showAtribute(){
-    let statStr = personaje[0].valor;
-    let modStr = personaje[0].modificador;
+    atriget = JSON.parse( localStorage.getItem('atriset').split(","));
+    let statStr = atriget[0].valor;
+    let modStr = atriget[0].modificador;
     let nodoStr = document.createElement("div");
     nodoStr.innerHTML =     ` ${statStr}
                             <h3>Modificador</h3>
                             <div> ${modStr} </div>`;
     document.getElementById("tituloStr").appendChild(nodoStr);
-    let statDex = personaje[1].valor;
-    let modDex = personaje[1].modificador;
+    let statDex = atriget[1].valor;
+    let modDex = atriget[1].modificador;
     let nodoDex = document.createElement("div");
     nodoDex.innerHTML = ` ${statDex}
                         <h3>Modificador</h3>
                         <div> ${modDex} </div>`;
     document.getElementById("tituloDex").appendChild(nodoDex);
-    let statCon = personaje[2].valor;
-    let modCon = personaje[2].modificador;
+    let statCon = atriget[2].valor;
+    let modCon = atriget[2].modificador;
     let nodoCon = document.createElement("div");
     nodoCon.innerHTML = ` ${statCon}
                         <h3>Modificador</h3>
                         <div> ${modCon} </div>`;
     document.getElementById("tituloCon").appendChild(nodoCon);
-    let statInt = personaje[3].valor;
-    let modInt = personaje[3].modificador;
+    let statInt = atriget[3].valor;
+    let modInt = atriget[3].modificador;
     let nodoInt = document.createElement("div");
     nodoInt.innerHTML = ` ${statInt}
                         <h3>Modificador</h3>
                         <div> ${modInt} </div>`;
     document.getElementById("tituloInt").appendChild(nodoInt);
-    let statWis = personaje[4].valor;
-    let modWis = personaje[4].modificador;
+    let statWis = atriget[4].valor;
+    let modWis = atriget[4].modificador;
     let nodoWis = document.createElement("div");
     nodoWis.innerHTML = ` ${statWis}
                         <h3>Modificador</h3>
                         <div> ${modWis} </div>`;
     document.getElementById("tituloWis").appendChild(nodoWis);
-    let statCha = personaje[5].valor;
-    let modCha = personaje[5].modificador;
+    let statCha = atriget[5].valor;
+    let modCha = atriget[5].modificador;
     let nodoCha = document.createElement("div");
     nodoCha.innerHTML = ` ${statCha}
                         <h3>Modificador</h3>
@@ -209,7 +213,7 @@ function dados(atri,val, modi){
 
 //Funcion que determina el exito de la tirada
 function tirada(){
-    final = Number(dice) + Number(personaje[stat].modificador);
+    final = Number(dice) + Number(atriget[stat].modificador);
     console.log(final);
     document.write("<br>" + "tu tirada es de: " + dice);
     switch (true){
@@ -239,7 +243,6 @@ function tirada(){
             break;
     }
 }
-
 
 //arregla el array de atributos usando los valores de mayor a manor
 let statsorter = personaje.sort((a, b) => b.valor - a.valor);
